8721285b0c4de8f8b219d3b227a4e5041efeb7f6
Cesar Alaestante <cesar@roci.dev>
2023-12-04 16:24:48 -0700

    working test

--- packages/reflect-yjs/src/awareness.test.ts
+++ packages/reflect-yjs/src/awareness.test.ts
@@ -1,112 +1,92 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import {expect} from 'chai';
 import {Awareness} from './awareness.js';
-import {
-  ConnectionState,
-  MockSocket,
-  TestReflect,
-  reflectForTest,
-  tickAFewTimes,
-} from './test-utils.js';
-import {
-  updateYJS,
-  yjsSetLocalState,
-  yjsSetLocalStateField,
-  Mutators,
-} from './mutators.js';
+import {resolver} from '@rocicorp/resolver';
+
+import {Mutators, mutators} from './mutators.js';
 import * as Y from 'yjs';
-import {
-  applyAwarenessUpdate,
-  encodeAwarenessUpdate,
-} from 'y-protocols/awareness.js';
+
 import * as t from 'lib0/testing.js';
-import * as sinon from 'sinon';
+import {Reflect} from '@rocicorp/reflect/client';
 
 let aw1: Awareness;
 let aw2: Awareness;
 
-let mockReflect: TestReflect<Mutators>;
+let reflect: Reflect<Mutators>;
 let doc1: Y.Doc;
 let doc2: Y.Doc;
 
-let clock: sinon.SinonFakeTimers;
-const startTime = 1678829450000;
-
-setup(async () => {
-  clock = sinon.useFakeTimers();
-  clock.setSystemTime(startTime);
-  sinon.replace(
-    globalThis,
-    'WebSocket',
-    MockSocket as unknown as typeof WebSocket,
-  );
-  mockReflect = reflectForTest({
-    mutators: {
-      yjsSetLocalStateField,
-      yjsSetLocalState,
-      updateYJS,
-    },
+setup(() => {
+  reflect = new Reflect<Mutators>({
+    server: undefined,
+    userID: '1',
+    roomID: '1',
+    mutators,
   });
-  console.log(clock.now);
-  await mockReflect.triggerConnected();
-  await mockReflect.waitForConnectionState(ConnectionState.Connected);
-  // await clock.tickAsync(0);
-  // await tickAFewTimes(clock);
-  expect(mockReflect.online).true;
+
   doc1 = new Y.Doc();
-  aw1 = new Awareness(mockReflect, 'testName', doc1);
+  doc1.clientID = 0;
+  aw1 = new Awareness(reflect, 'testName', doc1);
 
   doc2 = new Y.Doc();
-  aw2 = new Awareness(mockReflect, 'testName', doc2);
+  doc2.clientID = 1;
+  aw2 = new Awareness(reflect, 'testName', doc2);
 });
 
 teardown(() => {});
 
-test('Awareness', () => {
+test('Awareness', async () => {
   expect(aw1).to.be.an.instanceof(Awareness);
 
-  // Check initial state
   const initialState = aw1.getLocalState();
   expect(initialState).to.deep.equal(null);
 
-  console.log('aw1.getLocalState()', aw1.getLocalState());
-  aw1.setLocalStateField('user', {
-    avatar: 'random-avatar.png',
-    name: 'Test User',
-    color: '#000000',
+  const aw1changes: {added: number[]; updated: number[]; removed: number[]}[] =
+    [];
+  const aw1changeResolvers = [resolver<void>(), resolver<void>()];
+  let aw1changecalls = 0;
+
+  aw1.on('change', change => {
+    aw1changes.push(change);
+    aw1changeResolvers[aw1changecalls++].resolve();
+  });
+  const aw2changes: {added: number[]; updated: number[]; removed: number[]}[] =
+    [];
+  const aw2changeResolvers = [resolver<void>(), resolver<void>()];
+  let aw2changecalls = 0;
+  aw2.on('change', change => {
+    aw2changes.push(change);
+    aw2changeResolvers[aw2changecalls++].resolve();
   });
 
-  const doc1 = new Y.Doc();
-  doc1.clientID = 0;
-  const doc2 = new Y.Doc();
-  doc2.clientID = 1;
-  aw1.on('update', ({added, updated, removed}) => {
-    const enc = encodeAwarenessUpdate(
-      aw1,
-      added.concat(updated).concat(removed),
-    );
-    applyAwarenessUpdate(aw2, enc, 'custom');
-  });
-  let lastChangeLocal = null;
-  aw1.on('change', change => {
-    lastChangeLocal = change;
-  });
-  let lastChange = null;
-  aw2.on('change', change => {
-    lastChange = change;
-  });
+  await aw2changeResolvers[0].promise;
+  t.compare(aw1.getStates(), aw2.getStates());
+  t.compare(
+    aw2.getStates(),
+    new Map([
+      [0, {}],
+      [1, {}],
+    ]),
+  );
+
+  expect(aw1changes).to.deep.equal(aw2changes);
+  expect(aw2changes).to.deep.equal([{added: [0, 1], updated: [], removed: []}]);
+
   aw1.setLocalState({x: 3});
-  console.log('aw1.getStates()', aw1.getStates());
-  console.log('aw2.getStates()', aw2.getStates());
-  t.compare(aw2.getStates().get(0), {x: 3});
-  if (aw2.meta.get(0) !== undefined) {
-    //@ts-ignore
-    t.assert(aw2.meta.get(0).clock === 1);
-  }
-  if (lastChange !== null) {
-    //@ts-ignore
-    t.compare(lastChange.added, [0]);
-  }
-  // When creating an Awareness instance, the the local client is already marked as available, so it is not updated.
-  t.compare(lastChangeLocal, {added: [], updated: [0], removed: []});
+
+  await aw2changeResolvers[1].promise;
+  t.compare(aw1.getStates(), aw2.getStates());
+  t.compare(
+    aw2.getStates(),
+    new Map([
+      [0, {x: 3}],
+      [1, {}],
+    ]),
+  );
+
+  expect(aw1changes).to.deep.equal(aw2changes);
+  expect(aw2changes).to.deep.equal([
+    {added: [0, 1], updated: [], removed: []},
+    {added: [], updated: [0], removed: []},
+  ]);
 });
