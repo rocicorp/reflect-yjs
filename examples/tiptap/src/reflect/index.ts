a2d84d174ae5705a4dda91e5769c9550b2d3444d
Cesar Alaestante <cesar@roci.dev>
2023-12-18 16:43:23 -0700

    default text for room

--- examples/tiptap/src/reflect/index.ts
+++ examples/tiptap/src/reflect/index.ts
@@ -3,6 +3,7 @@ import type {
   ReflectServerOptions,
 } from "@rocicorp/reflect/server";
 import { mutators as yjsMutators, Mutators } from "@rocicorp/reflect-yjs";
+import { WriteTransaction } from "@rocicorp/reflect";
 
 const authHandler: AuthHandler = (auth: string, _roomID: string) => {
   if (auth) {
@@ -24,6 +25,14 @@ function makeOptions(): ReflectServerOptions<Mutators> {
   return {
     mutators: yjsMutators,
     authHandler,
+    roomStartHandler: async (tx: WriteTransaction, _roomID: string) => {
+      await yjsMutators.updateYJS(tx, {
+        name: "one",
+        id: crypto.randomUUID,
+        update:
+          "AAMAAwAGwcyjqQ4ICgACQgAyAkKKFQIAEQcBBAAoAIcABwAEAIcABwAExQe5B2RlZmF1bHRoZWFkaW5nQ2hhcHRlciBUd2VsdmU6IEhvbGRlbmxldmVscGFyYWdyYXBoVGhlIFRyYW5zcG9ydCBVbmlvbiBjb21wdHJvbGxlcuKAmXMgb2ZmaWNlcyB3ZXJlIGJ1cmllZCB0aHJlZSBsZXZlbHMgZGVlcCBpbiB0aGUgdGhpY2sgd2FsbHMgb2YgTWVkaW5hIFN0YXRpb27igJlzIHJvdGF0aW5nIGRydW0uIEl0IG1hZGUgdGhlIENvcmlvbGlzIHNsaWdodGx5IGxlc3Mgbm90aWNlYWJsZSB0aGFuIGluc2lkZSB0aGUgZHJ1bSwgYnV0IGFsc28gbWVhbnQgdGhhdCB0aGV5IHdlcmUgaW5zaWRlIGdyYXkgbWV0YWwgY3ViZXMgd2l0aCBkZXNrcyBpbiB0aGVtIGFuZCBubyBzY3JlZW5zIHRvIGV2ZW4gZ2l2ZSB0aGUgaWxsdXNpb24gb2YgYSB3aW5kb3cuIEhvbGRlbiBjb3VsZG7igJl0IHNheSBmb3Igc3VyZSB3aHkgaXQgZmVsdCBtb3JlIGRlcHJlc3NpbmcgdGhhbiBzaXR0aW5nIGluIHRoZSBtZXRhbCBjdWJlcyBvZiBhIHNwYWNlc2hpcCBjb21wYXJ0bWVudCwgYnV0IGl0IHdhcy4gTmFvbWkgc2F0IGJlc2lkZSBoaW0sIHdhdGNoaW5nIHRoZSBuZXdzZmVlZHMgb24gaGVyIGhhbmQgdGVybWluYWwsIHVuYWZmZWN0ZWQgYnkgdGhlIGdyaW0gbG9jYWxlLiBUaGUgUm9jaW5hbnRlIHdhcyBkb2luZyBhIG1hbmRhdG9yeSBzZWN1cml0eSBjb250cmFjdC4gVGhlIGZpcnN0IGdpZyBzaW5jZSB0aGV54oCZZCBsZWZ0LiBNYXliZSB0aGF0IHdhcyB3aGF0IGhlIHdhcyByZWFjdGluZyB0by5wYXJhZ3JhcGjigJxGb3JtIDQwMTEtRCB0cmFuc2ZlcnMgeW91ciByZXRhaW5lciBhbmQgZnV0dXJlIGNvbnRyYWN0cyB0byBSb2JlcnRhIFcuIERyYXBlciwgYW5kIHN0YXRlcyB0aGF0IHNoZSBpcyBub3cgdGhlIGxlZ2FsIGNhcHRhaW4gb2YgdGhlIFJvY2luYW50ZSwgYW5kIHByZXNpZGVudCBvZiBSb2NpY29ycCwgYSBDZXJlcy1yZWdpc3RlcmVkIGNvcnBvcmF0ZSBlbnRpdHku4oCdRwAWBQmjCgmPAwMBAAAGAwYDBgMGAQEBCgB9AQA=",
+      });
+    },
     logLevel: "debug",
   };
 }
